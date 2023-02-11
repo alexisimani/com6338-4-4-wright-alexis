0832c182f7e716c937ec46543863a8d56168281a
AlexisW <alexiswright@ufl.edu>
2023-02-10 22:47:08 -0500

    commit

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+var wordtoguessEl = document.getElementById('word-to-guess')
+var previouswordEl = document.getElementById('previous-word')
+var incorrectlettersEl = document.getElementById('incorrect-letters')
+var remainingguessesEl = document.getElementById('remaining-guesses')
+var winsEl = document.getElementById('wins')
+var lossesEl = document.getElementById('losses')
+
+var correct = 0
+var incorrect = 0
+
 var words = [
   'bananas',
   'grapes',
@@ -10,4 +20,30 @@ var words = [
   'meatloaf',
   'ukulele',
   'mango'
-]
+];
+
+var correctword = [];
+
+var game = {
+  pickWord() {
+    var randomWord = words[Math.floor(Math.random() * words.length)];
+    correctword.length = randomWord.length;
+    correctword.fill('_');
+    wordtoguessEl.innerHTML = correctword.join(' ');
+    this.chosenWord = randomWord;
+  },
+
+  validateEntry(key) {
+    if (
+    this.chosenWord.indexOf(key) > -1 || 
+    this.chosenWord.indexOf(key.toLowerCase()) > -1
+    );
+  }
+}
+
+game.pickWord();
+
+document.onkeyup = function(e){
+  console.log(e.key) 
+}
+
